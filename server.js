8a048d163761ad114e24de2ba1709c2d13d95b72
Thomas Calle <thos.calle@gmail.com>
2023-03-20 17:48:50 -0400

    foundation

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = require('./routes');
+// Import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// Sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
+
+// Thank you for viewing!
+// Thomas Calle 😊
